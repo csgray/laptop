01af1d01e9c8b68abc1bad4ac8a14ff7ae8e562a
Corey Gray <csgray@alaska.edu>
2019-02-01 15:05:42 -0900

    Add aliases for python3

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,6 +7,13 @@
 package 'git'
 package 'bolt'
 
+cookbook_file '/home/csgray/.bashrc' do
+  source 'bashrc'
+  owner 'csgray'
+  group 'csgray'
+  mode '0644'
+end
+
 include_recipe 'laptop::nvidia'
 include_recipe 'laptop::vscode'
 include_recipe 'laptop::backgrounds'
