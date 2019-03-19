0e133204505d4ab3427172af0408b6d99eeda9a2
Corey Gray <csgray@alaska.edu>
2019-03-18 17:04:23 -0800

    Add Yubikey two-factor authentication.

--- recipes/default.rb
+++ recipes/default.rb
@@ -14,6 +14,14 @@ cookbook_file '/home/csgray/.bashrc' do
   mode '0644'
 end
 
+# Enables Yubikey two-factor authentication
+remote_file '/etc/udev/rules.d/70-u2f.rules' do
+  source 'https://raw.githubusercontent.com/Yubico/libu2f-host/master/70-u2f.rules'
+  owner 'root'
+  group 'root'
+  mode '0644'
+end
+
 include_recipe 'laptop::nvidia'
 include_recipe 'laptop::vscode'
 include_recipe 'laptop::backgrounds'
