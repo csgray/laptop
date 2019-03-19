197498d8fe4db41bcd07b37e4f0d9e31dd663e54
Corey Gray <csgray@alaska.edu>
2019-03-18 17:40:01 -0800

    Add more backgrounds after completing patch 2.3

--- recipes/backgrounds.rb
+++ recipes/backgrounds.rb
@@ -11,7 +11,8 @@ directory '/home/csgray/Pictures/FF14' do
   action :create
 end
 
-remote_file '/home/csgray/Pictures/FF14/RealmReborn1.jpg' do
+# Final Fantasy XIV: A Realm Reborn general backgrounds
+remote_file '/home/csgray/Pictures/FF14/ARealmReborn01.jpg' do
   source 'https://img.finalfantasyxiv.com/lds/h/R/ie9yzPWPiNVaZecpAaFODyYLxw.jpg'
   owner 'csgray'
   group 'csgray'
@@ -19,7 +20,7 @@ remote_file '/home/csgray/Pictures/FF14/RealmReborn1.jpg' do
   action :create
 end
 
-remote_file '/home/csgray/Pictures/FF14/RealmReborn2.jpg' do
+remote_file '/home/csgray/Pictures/FF14/ARealmReborn02.jpg' do
   source 'https://img.finalfantasyxiv.com/lds/h/l/QpExCxHkd7zpxqXK1cl4Uz8bJ8.jpg'
   owner 'csgray'
   group 'csgray'
@@ -27,7 +28,64 @@ remote_file '/home/csgray/Pictures/FF14/RealmReborn2.jpg' do
   action :create
 end
 
-remote_file '/home/csgray/Pictures/FF14/RealmAwoken1.jpg' do
+remote_file '/home/csgray/Pictures/FF14/ARealmReborn03.jpg' do
+  source 'https://img.finalfantasyxiv.com/lds/h/F/wQiRqguA6t98BwLN6tHhfjPQEo.jpg'
+  owner 'csgray'
+  group 'csgray'
+  mode '0755'
+  action :create
+end
+
+remote_file '/home/csgray/Pictures/FF14/ARealmReborn04.jpg' do
+  source 'https://img.finalfantasyxiv.com/lds/h/8/IfFUYSe8TMK6fF_Z_6mk0izD-Q.jpg'
+  owner 'csgray'
+  group 'csgray'
+  mode '0755'
+  action :create
+end
+
+remote_file '/home/csgray/Pictures/FF14/ARealmReborn05.jpg' do
+  source 'https://img.finalfantasyxiv.com/lds/h/O/b7wgHLVsVoevO4GcPzE4NUARiU.jpg'
+  owner 'csgray'
+  group 'csgray'
+  mode '0755'
+  action :create
+end
+
+remote_file '/home/csgray/Pictures/FF14/ARealmReborn06.jpg' do
+  source 'https://img.finalfantasyxiv.com/lds/h/0/lDoqSVc4nCwwbR_Y0JbqzHlcTc.jpg'
+  owner 'csgray'
+  group 'csgray'
+  mode '0755'
+  action :create
+end
+
+remote_file '/home/csgray/Pictures/FF14/ARealmReborn07.jpg' do
+  source 'https://img.finalfantasyxiv.com/lds/h/Z/mV_xbsCMxhEdmODtNw_Q3WyN_c.jpg'
+  owner 'csgray'
+  group 'csgray'
+  mode '0755'
+  action :create
+end
+
+remote_file '/home/csgray/Pictures/FF14/ARealmReborn08.jpg' do
+  source 'https://img.finalfantasyxiv.com/lds/h/E/2sIEKyoyFa_hy-mbn5u1-NZof4.jpg'
+  owner 'csgray'
+  group 'csgray'
+  mode '0755'
+  action :create
+end
+
+remote_file '/home/csgray/Pictures/FF14/ARealmReborn09.jpg' do
+  source 'https://img.finalfantasyxiv.com/lds/h/0/nBhToU_Ek67f7CBtSSatoStYrk.jpg'
+  owner 'csgray'
+  group 'csgray'
+  mode '0755'
+  action :create
+end
+
+# Final Fantasy XIV: A Realm Reborn patch-specific backgrounds
+remote_file '/home/csgray/Pictures/FF14/ARealmAwoken01.jpg' do
   source 'https://img.finalfantasyxiv.com/lds/h/4/RGmM1Gbf-9ZPnaTdKohVIPO6KE.jpg'
   owner 'csgray'
   group 'csgray'
@@ -35,7 +93,7 @@ remote_file '/home/csgray/Pictures/FF14/RealmAwoken1.jpg' do
   action :create
 end
 
-remote_file '/home/csgray/Pictures/FF14/RealmAwoken2.jpg' do
+remote_file '/home/csgray/Pictures/FF14/ARealmAwoken02.jpg' do
   source 'https://img.finalfantasyxiv.com/lds/h/t/z4U_Gy3ywj1qzbfD_iE2CblZ84.jpg'
   owner 'csgray'
   group 'csgray'
@@ -43,7 +101,7 @@ remote_file '/home/csgray/Pictures/FF14/RealmAwoken2.jpg' do
   action :create
 end
 
-remote_file '/home/csgray/Pictures/FF14/RealmAwoken3.jpg' do
+remote_file '/home/csgray/Pictures/FF14/ARealmAwoken03.jpg' do
   source 'https://img.finalfantasyxiv.com/lds/h/V/tldeK1gTo_yrucUnOqStlAO020.jpg'
   owner 'csgray'
   group 'csgray'
@@ -51,7 +109,7 @@ remote_file '/home/csgray/Pictures/FF14/RealmAwoken3.jpg' do
   action :create
 end
 
-remote_file '/home/csgray/Pictures/FF14/RealmAwoken4.jpg' do
+remote_file '/home/csgray/Pictures/FF14/ARealmAwoken04.jpg' do
   source 'https://img.finalfantasyxiv.com/lds/h/E/-qRcuAOPrQ_dQEg05rnr-BLyok.jpg'
   owner 'csgray'
   group 'csgray'
@@ -59,7 +117,7 @@ remote_file '/home/csgray/Pictures/FF14/RealmAwoken4.jpg' do
   action :create
 end
 
-remote_file '/home/csgray/Pictures/FF14/RealmAwoken5.jpg' do
+remote_file '/home/csgray/Pictures/FF14/ARealmAwoken05.jpg' do
   source 'https://img.finalfantasyxiv.com/lds/h/b/UZKvEQMlpClpCM0KKBs1MYWMcM.jpg'
   owner 'csgray'
   group 'csgray'
@@ -67,7 +125,7 @@ remote_file '/home/csgray/Pictures/FF14/RealmAwoken5.jpg' do
   action :create
 end
 
-remote_file '/home/csgray/Pictures/FF14/RealmAwoken6.jpg' do
+remote_file '/home/csgray/Pictures/FF14/ARealmAwoken06.jpg' do
   source 'https://img.finalfantasyxiv.com/lds/h/6/VwMh0mTCmIA6jhdpLLQIytydkg.jpg'
   owner 'csgray'
   group 'csgray'
@@ -75,7 +133,7 @@ remote_file '/home/csgray/Pictures/FF14/RealmAwoken6.jpg' do
   action :create
 end
 
-remote_file '/home/csgray/Pictures/FF14/ThroughTheMaelstrom1.jpg' do
+remote_file '/home/csgray/Pictures/FF14/ThroughTheMaelstrom01.jpg' do
   source 'https://img.finalfantasyxiv.com/lds/h/T/B1HoVJIeln6oyjNFMQQ6Ua44qQ.jpg'
   owner 'csgray'
   group 'csgray'
@@ -83,7 +141,7 @@ remote_file '/home/csgray/Pictures/FF14/ThroughTheMaelstrom1.jpg' do
   action :create
 end
 
-remote_file '/home/csgray/Pictures/FF14/ThroughTheMaelstrom2.jpg' do
+remote_file '/home/csgray/Pictures/FF14/ThroughTheMaelstrom02.jpg' do
   source 'https://img.finalfantasyxiv.com/lds/h/T/Du69iapLR8aMGSq7wYNmiOBeKE.jpg'
   owner 'csgray'
   group 'csgray'
@@ -91,7 +149,7 @@ remote_file '/home/csgray/Pictures/FF14/ThroughTheMaelstrom2.jpg' do
   action :create
 end
 
-remote_file '/home/csgray/Pictures/FF14/ThroughTheMaelstrom3.jpg' do
+remote_file '/home/csgray/Pictures/FF14/ThroughTheMaelstrom03.jpg' do
   source 'https://img.finalfantasyxiv.com/lds/h/v/mJPynE5K_9dk9yWvKOFD3qLf9w.jpg'
   owner 'csgray'
   group 'csgray'
@@ -99,8 +157,177 @@ remote_file '/home/csgray/Pictures/FF14/ThroughTheMaelstrom3.jpg' do
   action :create
 end
 
-remote_file '/home/csgray/Pictures/FF14/ThroughTheMaelstrom4.jpg' do
+remote_file '/home/csgray/Pictures/FF14/ThroughTheMaelstrom04.jpg' do
   source 'https://img.finalfantasyxiv.com/lds/h/U/0P1ncRVYw3wO_6OEYE375vk-0I.jpg'
+  owner 'csgray'
+  group 'csgray'
+  mode '0755'
+  action :create
+end
+
+remote_file '/home/csgray/Pictures/FF14/DefendersOfEorzea01.jpg' do
+  source 'https://img.finalfantasyxiv.com/lds/h/K/yhv5Ppgen4PVXYqPIqrXWlrCh0.jpg'
+  owner 'csgray'
+  group 'csgray'
+  mode '0755'
+  action :create
+end
+
+remote_file '/home/csgray/Pictures/FF14/DefendersOfEorzea02.jpg' do
+  source 'https://img.finalfantasyxiv.com/lds/h/n/07wAxzpg3igFxcvpaE9xv8qA6M.jpg'
+  owner 'csgray'
+  group 'csgray'
+  mode '0755'
+  action :create
+end
+
+# Final Fantasy XIV: A Realm Reborn anniversary backgrounds
+remote_file '/home/csgray/Pictures/FF14/FirstAnniversary01.jpg' do
+  source 'https://img.finalfantasyxiv.com/lds/h/-/SGqYV1zKxXQPF9Jol9ZynUJTyo.jpg'
+  owner 'csgray'
+  group 'csgray'
+  mode '0755'
+  action :create
+end
+
+remote_file '/home/csgray/Pictures/FF14/FirstAnniversary02.jpg' do
+  source 'https://img.finalfantasyxiv.com/lds/h/_/8UFhHxA02QicLhNct8VcUC6LRA.jpg'
+  owner 'csgray'
+  group 'csgray'
+  mode '0755'
+  action :create
+end
+
+remote_file '/home/csgray/Pictures/FF14/FirstAnniversary03.jpg' do
+  source 'https://img.finalfantasyxiv.com/lds/h/5/omf_RX_MTk1Hv-PaRoQwNRCTOM.jpg'
+  owner 'csgray'
+  group 'csgray'
+  mode '0755'
+  action :create
+end
+
+remote_file '/home/csgray/Pictures/FF14/FirstAnniversary04.jpg' do
+  source 'https://img.finalfantasyxiv.com/lds/h/b/1LcKfrXTKHt7oL4PR8qT_SUG2A.jpg'
+  owner 'csgray'
+  group 'csgray'
+  mode '0755'
+  action :create
+end
+
+remote_file '/home/csgray/Pictures/FF14/FirstAnniversary05.jpg' do
+  source 'https://img.finalfantasyxiv.com/lds/h/W/PO06A8Q19rShi2-Q9M0YZIcOko.jpg'
+  owner 'csgray'
+  group 'csgray'
+  mode '0755'
+  action :create
+end
+
+remote_file '/home/csgray/Pictures/FF14/FirstAnniversary06.jpg' do
+  source 'https://img.finalfantasyxiv.com/lds/h/S/Ad5-rOGrVJdCc0W-IcmbiXt6w8.jpg'
+  owner 'csgray'
+  group 'csgray'
+  mode '0755'
+  action :create
+end
+
+remote_file '/home/csgray/Pictures/FF14/FirstAnniversary07.jpg' do
+  source 'https://img.finalfantasyxiv.com/lds/h/a/VBBXHUhFm12VPBBEBtM_znnqNQ.jpg'
+  owner 'csgray'
+  group 'csgray'
+  mode '0755'
+  action :create
+end
+
+remote_file '/home/csgray/Pictures/FF14/FirstAnniversary08.jpg' do
+  source 'https://img.finalfantasyxiv.com/lds/h/_/uuKyB_GDxi05zddcpRiwG3IWkQ.jpg'
+  owner 'csgray'
+  group 'csgray'
+  mode '0755'
+  action :create
+end
+
+remote_file '/home/csgray/Pictures/FF14/FirstAnniversary09.jpg' do
+  source 'https://img.finalfantasyxiv.com/lds/h/3/P0oigJ1A2iOAPAq8M7OJUR-bJo.jpg'
+  owner 'csgray'
+  group 'csgray'
+  mode '0755'
+  action :create
+end
+
+remote_file '/home/csgray/Pictures/FF14/FirstAnniversary10.jpg' do
+  source 'https://img.finalfantasyxiv.com/lds/h/y/AmZZcmNtesDf6PwJm0tQ8YOLzo.jpg'
+  owner 'csgray'
+  group 'csgray'
+  mode '0755'
+  action :create
+end
+
+remote_file '/home/csgray/Pictures/FF14/FirstAnniversary11.jpg' do
+  source 'https://img.finalfantasyxiv.com/lds/h/N/Al4Rave8fTisrx_QUo8jNjcBns.jpg'
+  owner 'csgray'
+  group 'csgray'
+  mode '0755'
+  action :create
+end
+
+remote_file '/home/csgray/Pictures/FF14/FirstAnniversary12.jpg' do
+  source 'https://img.finalfantasyxiv.com/lds/h/w/Dj58_xkOpEhu6Z_i0Ey_IPLIE0.jpg'
+  owner 'csgray'
+  group 'csgray'
+  mode '0755'
+  action :create
+end
+
+remote_file '/home/csgray/Pictures/FF14/FirstAnniversary13.jpg' do
+  source 'https://img.finalfantasyxiv.com/lds/h/X/GPCsx3gzuZcRcP2pNrQxEsCnWE.jpg'
+  owner 'csgray'
+  group 'csgray'
+  mode '0755'
+  action :create
+end
+
+remote_file '/home/csgray/Pictures/FF14/FirstAnniversary14.jpg' do
+  source 'https://img.finalfantasyxiv.com/lds/h/4/F86ze-OKLMU3WcpQxw3xggMbsk.jpg'
+  owner 'csgray'
+  group 'csgray'
+  mode '0755'
+  action :create
+end
+
+remote_file '/home/csgray/Pictures/FF14/FirstAnniversary15.jpg' do
+  source 'https://img.finalfantasyxiv.com/lds/h/3/tQxPH7wo7fxFzc5Zov-oofT86I.jpg'
+  owner 'csgray'
+  group 'csgray'
+  mode '0755'
+  action :create
+end
+
+remote_file '/home/csgray/Pictures/FF14/FirstAnniversary16.jpg' do
+  source 'https://img.finalfantasyxiv.com/lds/h/Z/AyMCyI1N5BE7i_-MDPXrujDc7o.jpg'
+  owner 'csgray'
+  group 'csgray'
+  mode '0755'
+  action :create
+end
+
+remote_file '/home/csgray/Pictures/FF14/FirstAnniversary17.jpg' do
+  source 'https://img.finalfantasyxiv.com/lds/h/k/EP6-E5JRozd0rbA9JtnHDtgY80.jpg'
+  owner 'csgray'
+  group 'csgray'
+  mode '0755'
+  action :create
+end
+
+remote_file '/home/csgray/Pictures/FF14/FirstAnniversary18.jpg' do
+  source 'https://img.finalfantasyxiv.com/lds/h/G/FdU41q2WaNNcurQTEzwZVcfBgg.jpg'
+  owner 'csgray'
+  group 'csgray'
+  mode '0755'
+  action :create
+end
+
+remote_file '/home/csgray/Pictures/FF14/FirstAnniversary19.jpg' do
+  source 'https://img.finalfantasyxiv.com/lds/h/T/qLC1IvyPVh45Sim5YMAKYhWrFU.jpg'
   owner 'csgray'
   group 'csgray'
   mode '0755'
